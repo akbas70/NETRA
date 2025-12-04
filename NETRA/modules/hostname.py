614da89b11aa58853eaf5482c8b6db0ce1150035
nexucy19 <lantrinh1245@gmail.com>
2025-12-04 15:45:19 +0700

    Delete NETRA/modules/hostname.py

--- NETRA/modules/hostname.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def get_hostname(ip):
-    """Get hostname from IP, returns None if not resolvable."""
-    try:
-        return socket.gethostbyaddr(ip)[0]
-    except:
-        return None
